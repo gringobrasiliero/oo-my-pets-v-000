0d6374e8836f1f37b1c9ce73ef87c3ae122af042
Nolan Byrnes <nolanehs@gmail.com>
2017-11-23 14:32:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -32,11 +32,11 @@ def buy_fish(name)
 pets[:fishes] << Fish.new(name)
 end
 
-def buy_cat(cat)
+def buy_cat(name)
   pets[:cats] << Cat.new(name)
 end
 
-def buy_dog(dog)
+def buy_dog(name)
   pets[:dogs] << Dog.new(name)
 end
 
